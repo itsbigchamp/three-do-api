6c0a5f4a31e55c715b009455763be909b24d0b12
Gavin Anderson <btcgrowth1@gmail.com>
2022-08-26 10:52:16 -0400

    three do api start

---
+++ functions/src/dbConnect.js
@@ -0,0 +1,13 @@
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { credentials } from "./credentials";
+
+export default function dbConnect() {
+
+  if (!getApps().length) {
+    initializeApp({
+        credential: cert(credentials)
+    });
+ }
+ return getFirestore
+}
